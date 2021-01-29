80c88b209f06c5b07f9e6d583526525ea0270124
Jens W. Klein <jk@kleinundpartner.at>
2021-01-29 12:44:37 +0100

    explicitly add plone 6 to trove

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Framework :: Plone",
         "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
         "Framework :: Plone :: Core",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python",
